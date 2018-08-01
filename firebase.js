695c915fb86811b12ac6b31f6a1e85b35e4cc614
evelinj41 <39770138+evelinj41@users.noreply.github.com>
2018-08-01 17:17:45 -0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyClhlxMkx7r6Ph0B7o_Y0iaNvzS2Rqjt78",
+    authDomain: "homeworkce-e9008.firebaseapp.com",
+    databaseURL: "https://homeworkce-e9008.firebaseio.com",
+    projectId: "homeworkce-e9008",
+    storageBucket: "homeworkce-e9008.appspot.com",
+    messagingSenderId: "1077796373586"
+  };
+  firebase.initializeApp(config);
+  const database = firebase.database();
+
+   var name = $ ('.name').val();
+   var number = $ ('.number').val();
+   var lugares = $ ('.container').val();

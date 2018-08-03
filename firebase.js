400ac734a394c9ff94faf5ddbabb51ff3f78511f
CristianHolguin14 <39769914+CristianHolguin14@users.noreply.github.com>
2018-08-03 18:08:31 -0500

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,14 +1,13 @@
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyClhlxMkx7r6Ph0B7o_Y0iaNvzS2Rqjt78",
-    authDomain: "homeworkce-e9008.firebaseapp.com",
-    databaseURL: "https://homeworkce-e9008.firebaseio.com",
-    projectId: "homeworkce-e9008",
-    storageBucket: "homeworkce-e9008.appspot.com",
-    messagingSenderId: "1077796373586"
-  };
-  firebase.initializeApp(config);
-  const database = firebase.database();
+var config = {
+  apiKey: "AIzaSyClhlxMkx7r6Ph0B7o_Y0iaNvzS2Rqjt78",
+  authDomain: "homeworkce-e9008.firebaseapp.com",
+  databaseURL: "https://homeworkce-e9008.firebaseio.com",
+  projectId: "homeworkce-e9008",
+  storageBucket: "homeworkce-e9008.appspot.com",
+  messagingSenderId: "1077796373586"
+};
+firebase.initializeApp(config);
+const database = firebase.database();
 
 $('.button').on('click', function( event ){
   event.preventDefault();
@@ -42,3 +41,43 @@ $('.button').on('click', function( event ){
     };
   });
 }); 
+
+ $("button").click( function( event ) {
+event.preventDefault();
+var mensaje =  $('#mensaje').val();
+
+var data = { usuario: username, mensaje:mensaje};
+database.ref('chat/').push(data, function(err) {
+if (err) { throw err; }
+else {
+  console.info( 'guardamos la informacion' );
+  ponerMensaje(data);
+    $('#mensaje').val('')
+    }
+  }); 
+});
+
+  function ponerMensaje( pepito ) {
+    $('#caja').append( '<p>' + pepito.usuario + ': ' + pepito.mensaje + '<p>');
+  }
+
+function iterar(data) {
+  for ( var chiguiro in data ){
+    if (data.hasOwnProperty( chiguiro ) ) {
+      var element = data[chiguiro];
+      var gato = { usuario: element.usuario, mensaje: element.mensaje};
+        ponerMensaje( gato );
+    }
+  }
+}
+
+  var traerMensajes = new Promise(function(res,rej){
+    var mensajes = database.ref('/chat/').once('value').then(function(snapshot){
+      return res( snapshot.val() );
+  });
+    if (!mensajes) { return rej(); }
+  });
+
+  traerMensajes.then(function (data){
+    iterar(data);
+  });
